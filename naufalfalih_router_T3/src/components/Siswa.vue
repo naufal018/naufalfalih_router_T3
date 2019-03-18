b2c411f62bb930072297e1fbffb0a59cf2f6c924
naufal018 <48674756+naufal018@users.noreply.github.com>
2019-03-18 16:45:14 +0700

    Add files via upload

---
+++ naufalfalih_router_T3/src/components/Siswa.vue
@@ -0,0 +1,29 @@
+<template>
+ <div><br>
+  <center>
+  <table border="1">
+    <tr>
+      <td><center><b>Kelas</b></center></td>
+      <td><center><b>Nama</b></center></td>
+    </tr>
+    <tr>
+      <td>XII - RPL 1</td>
+      <td>Naufal Falih</td>
+    </tr>
+    <tr>
+      <td>XII - TITL 2</td>
+      <td>geby dols</td>
+    </tr>
+    <tr>
+      <td>XII - TITL 3</td>
+      <td>bento</td>
+    </tr>
+  </table>
+  </center>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
